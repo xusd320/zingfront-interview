3d157b4c44ba012d3abedde0670b9c6f6e683575
xusd320 <xusd320@gmail.com>
2019-03-29 20:24:43 +0800

    update

--- btree.py
+++ btree.py
@@ -1,10 +1,12 @@
 class node():
+    # 二叉树节点
     def __init__(self, val = None, left = None, right = None):
         self.val   = val
         self.left  = left
         self.right = right
 
 def matrixTree(matrix):
+    # 由矩阵生成二叉树
     if len(matrix) == 0:
         raise Exception('Empty matrix')
     elif len(matrix[0]) == 0:
@@ -12,13 +14,17 @@ def matrixTree(matrix):
     m = len(matrix)
     n = len(matrix[0])
 
+    #取矩阵左上角为二叉树根节点
     root = node({'x' : 0, 'y' : 0, 'data' : matrix[0][0]})
     def genTree(root):
         x,y = root.val['x'], root.val['y']
+        #若(x,y)为矩阵内部点,则root的左子树为矩阵该点下方相邻点生成的二叉树，右子树为矩阵该点右方相邻点生成的二叉树 
         if x < m -1 and y < n -1:
             root.left  = node(
                 val   = {'x' : x + 1, 'y' : y, 'data' :  matrix[x + 1][y]}, 
+                #递归生成左子树
                 left  = genTree(node(val = { 'x' : x + 2, 'y' : y, 'data' : matrix[x + 2][y]})) if x < m -2 else None,
+                #递归生成右子树
                 right = genTree(node(val = { 'x' : x + 1, 'y' : y + 1, 'data' : matrix[x + 1][y + 1] })) 
             )
             
@@ -31,6 +37,7 @@ def matrixTree(matrix):
             return root
 
         elif x < m -1:
+            #若(x,y)为矩阵右边界点，则有左子树无右子树
             root.left  = node(
                 val   = {'x' : x + 1, 'y' : y, 'data' :  matrix[x + 1][y]},
                 left  = genTree(node(val = { 'x' : x + 2, 'y' : y, 'data' : matrix[x + 2][y]})) if x < m -2 else None,
@@ -39,6 +46,7 @@ def matrixTree(matrix):
             return root
 
         elif y < n -1:
+            #若(x,y)为矩阵下边界点，则有右子树无左子树
             root.right= node(
                 val   = {'x' : x, 'y' : y + 1, 'data' :  matrix[x][y + 1]},
                 right = genTree(node(val = { 'x' : x, 'y' : y + 2, 'data' : matrix[x][y + 2]})) if y < n -2 else None
@@ -47,7 +55,7 @@ def matrixTree(matrix):
             return root
 
         else: 
-           
+            #(x,y)为矩阵右下顶点， 返回叶子节点
             return node({'x' : x, 'y' : y, 'data' : matrix[x][y]})
 
     return genTree(root)
--- wzry.py
+++ wzry.py
@@ -1,5 +1,6 @@
 from btree import matrixTree 
 
+#英雄配置
 heros = [
     { 'name' : 'mengqi',      'life' : 10000, 'attack' : 100  },
     { 'name' : 'chengyaojin', 'life' : 5000,  'attack' : 200  },
@@ -8,6 +9,7 @@ heros = [
     { 'name' : 'direnjie',    'life' : 100,   'attack' : 2000 }
 ]
 
+#地图配置
 gameMap = [
     [133  , -523 , -558 , 846  , -907 , -1224, -1346, 787  , -411 , -1826],
     [-1478, -853 , -1401, 341  , -26  , 759  , -444 , 174  , -1594, -2000],
@@ -21,15 +23,21 @@ gameMap = [
     [-1494, -632 , 243  , 90   , 993  , 322  , 32   , -388 , -225 , 952  ]
 ]
 
+#遍历地图二叉树，生产英雄能赢得游戏所走过的路径，并算的最终血量, 利用栈进行深度遍历
 def btreepaths(hero, gameTree):
     if not gameTree:
         return []
+
+    #路劲数组，根节点入栈
     res, stack = [], [(gameTree, [])]
     while stack:
+        #栈弹出
         node, ls = stack.pop()
+        #计算根节点沿当前路径到当前节点的数值之和，并且加上英雄初始血量，为英雄当前血量，若该值不大于0，则英雄死亡，跳出当前分支，遍历其他路径
         if sum(map(lambda item : item['data'], ls)) <= - node.val['data'] - hero['life']:
             continue
         if not node.left and not node.right:
+            #此时英雄到达叶子节点即敌方基地，往路径数据中添加一条路径并记录英雄血量
             res.append({'path' : ls + [node.val], 'life' : sum(map(lambda item : item['data'], ls)) + node.val['data'] + hero['life']})
         if node.left:
             stack.append((node.left, ls + [ node.val ]))
@@ -40,10 +48,14 @@ def btreepaths(hero, gameTree):
 
 def heroChoice(heros, gameMap):
     result = {}
+    #按英雄攻击力从大到小排序
     heros.sort(key = lambda item : item['attack'], reverse = True)
     for hero in heros:
+        #由游戏地图生产二叉树，并搜索英雄通关路径,按英雄血量从大到小排序
         paths = btreepaths(hero, matrixTree(gameMap))
         paths.sort(key = lambda item : item['life'], reverse = True)
+
+        #若有通关路经，则选该英雄，并取最终血最多的路径为最优路径
         if paths:
             result['hero'] = hero
             result['path'] = paths[0] 
